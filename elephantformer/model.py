2b93f7a39ed691801545a2a0569ea1ddec4de149
adam-mehdi <62762546+adam-mehdi@users.noreply.github.com>
2021-08-26 09:29:07 -0700

    Update model.py

--- elephantformer/model.py
+++ elephantformer/model.py
@@ -7,7 +7,7 @@ from torch import nn, einsum
 import torch.nn.functional as F
 
 from einops import rearrange, repeat
-from elephantformer.rotary import AxialRotaryEmbedding
+from Elephantformer.rotary import AxialRotaryEmbedding
 
 # helper classes
 
